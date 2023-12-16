6ca3045e9ef1b2d086a43adf5b2a9bbce4b1af10
delbertina <6349928+delbertina@users.noreply.github.com>
2023-12-15 23:19:54 -0600

    Create project initial files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Placeholder
+      </header>
+    </div>
+  );
+}
+
+export default App;
