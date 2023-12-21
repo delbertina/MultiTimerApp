89cb02b1f13ad4a84aaa0e58fa050ec9fe89c839
delbertina <6349928+delbertina@users.noreply.github.com>
2023-12-21 17:45:26 -0600

    Update milliseconds to time display

--- src/components/TimerButton.tsx
+++ src/components/TimerButton.tsx
@@ -11,8 +11,20 @@ const TimerButton: React.FC<TimerButtonProps> = (props) => {
 
   const [timeDisplay, setTimeDisplay] = useState('0');
 
+  // format the millisecond duration to min:sec format using pad function
+  // round up to the nearest second to avoid 00:00 for less than 1 second
+  const millisecondsToTime = (millis: number): string => {
+    const inputRounded = Math.ceil(millis / 1000) * 1000;
+    const pad = (n: number, z = 2) => ("00" + n).slice(-z);
+    return (
+      pad(((inputRounded % 3.6e6) / 6e4) | 0) +
+      ":" +
+      pad(((inputRounded % 6e4) / 1000) | 0)
+    );
+  }
+
   useEffect(() => {
-    setTimeDisplay(props.milliseconds.toString());
+    setTimeDisplay(millisecondsToTime(props.milliseconds));
   }, [props.milliseconds])
 
   return (
