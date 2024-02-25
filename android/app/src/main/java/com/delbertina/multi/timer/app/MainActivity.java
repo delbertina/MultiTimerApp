46b5bcd1efde42697634897d985c245b503763a7
delbertina <6349928+delbertina@users.noreply.github.com>
2024-02-24 21:19:50 -0600

    Fix build, update visual assets, and add privacy policy

--- android/app/src/main/java/com/delbertina/multi/timer/app/MainActivity.java
+++ android/app/src/main/java/com/delbertina/multi/timer/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.delbertina.multi.timer.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.delbertina.multi-timer-app',
+  appId: 'com.delbertina.multi.timer.app',
   appName: 'Multi Timer App',
   webDir: 'dist',
   server: {
